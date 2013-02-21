1cb343b77c849fc7a5a9842e61e560ddc341e6a3
modal_soul <modalsoul0215@gmail.com>
2013-02-21 23:31:03 +0900

    edit formatting process. not finished.

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -5,6 +5,7 @@ import java.io.StringReader
 import scala.xml.Text
 import scala.xml.Node
 import scala.xml.parsing.NoBindingFactoryAdapter
+import scala.collection.mutable.ListBuffer
 import nu.validator.htmlparser.sax.HtmlParser
 import nu.validator.htmlparser.common.XmlViolationPolicy
 import org.xml.sax.InputSource
@@ -17,15 +18,40 @@ object Hello {
         
     val tableNode = (toNode(str) \\ "table").filter(_ \ "@class" contains Text("timetable"))
     
-    val trNodes = (tableNode \\ "tr").filter(_ \\ "th" \ "@class" contains Text("hour"))
+    val weekTypeNum = (tableNode \\ "tr" \\ "th").filter(_ \ "@class" contains Text("week")).size
     
-    trNodes.foreach { rowNode =>
-      var hour=(rowNode \\ "th").text + ", "
-      (rowNode \\ "td").foreach { tdNodes =>
-        tdNodes.foreach { tableData =>
-          println(hour + tableData.text.trim.replaceAll(" ", "").replaceAll("\n", ", "))
+    val hourList = (tableNode \\ "tr" \\ "th").filter(_ \ "@class" contains Text("hour"))
+    
+    val trNodeList = (tableNode \\ "tr").filter(_ \\ "th" \ "@class" contains Text("hour"))
+    
+    val timeList = Array.ofDim[String](weekTypeNum, 10)
+    
+    trNodeList.foreach { rowNode =>
+      var hour=(rowNode \\ "th").text
+      val tdNodeList = rowNode \\ "td"
+      
+      //(rowNode \\ "td").foreach { tdNodeList =>
+      for ( week <- 0 to weekTypeNum-1) {
+        //tdNodeList.foreach { tableData =>
+        val tableData = tdNodeList(week)
+          //val minuteList = tableData.text.replaceAll(" ", "").split("\n").filter(_ != "")
+        val minuteList = tableData.text.replaceAll(" ", "").split("\n").filter(_ != "")
+        for ( currentNum <- 0 to minuteList.size-1) {
+          
+          //minuteList.foreach { minute =>
+            //println(hour + ":" + time)
+          //  timeList(currentWeek)(num) = hour + ":" + minute 
+          //}
+          timeList(week)(currentNum) = hour + ":" + minuteList(currentNum)
+          //println(hour + tableData.text.trim.replaceAll(" ", "").replaceAll("\n", ", "))
         }
       }
+    }
+    
+    timeList.foreach { time =>
+    	time.foreach { smalltime =>
+    		println(smalltime)
+    	}
     }
     
     println("Hello Scala!")
