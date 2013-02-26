cf12f0c144ffb8f09b8a84ddae7b42964dce58c2
modal_soul <modalsoul0215@gmail.com>
2013-02-26 23:11:13 +0900

    change parsed time List to ArrayBuffer

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,11 +1,11 @@
-
 package scalamin
 
 import java.io.StringReader
 import scala.xml.Text
 import scala.xml.Node
 import scala.xml.parsing.NoBindingFactoryAdapter
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.ArrayBuffer
+import collection.mutable.{ArrayBuffer, ListBuffer}
 import nu.validator.htmlparser.sax.HtmlParser
 import nu.validator.htmlparser.common.XmlViolationPolicy
 import org.xml.sax.InputSource
@@ -15,53 +15,42 @@ import io.Source
 
 object Hello {
   def main(args:Array[String]) = {
-//    val str = FileUtils.readFileToString(new File("sample.xml"))
+    //    val str = FileUtils.readFileToString(new File("sample.xml"))
 
     val src = Source.fromURL("http://timetablenavi.keikyu-bus.co.jp/dia/timetable/web/51174/1020021012/08/", "Shift_JIS")
+
     val str = src.mkString
+
     val tableNode = (toNode(str) \\ "table").filter(_ \ "@class" contains Text("timetable"))
-    
+
     val weekTypeNum = (tableNode \\ "tr" \\ "th").filter(_ \ "@class" contains Text("week")).size
 
-    val hourNodeList = (tableNode \\ "tr" \\ "th").filter(_ \ "@class" contains Text("hour"))
-
-    
     val trNodeList = (tableNode \\ "tr").filter(_ \\ "th" \ "@class" contains Text("hour"))
-    
-    val timeList = Array.ofDim[String](weekTypeNum, hourNodeList.size, 10)
-    
+
+    val timeList = new ArrayBuffer[ArrayBuffer[String]]()
+
     trNodeList.foreach { rowNode =>
       val tdNodeList = rowNode \\ "td"
 
-      for ( currentWeek <- 0 to weekTypeNum-1) {
-        val minuteList = tdNodeList(currentWeek).text.replaceAll(" ", "").split("\n").filter(_ != "")
-        print(currentWeek)
-        minuteList.foreach { minute =>
-        	print("," + (rowNode \\ "th").text + ":" + minute)
+      for (week <- 0 to weekTypeNum-1) {
+        timeList += ArrayBuffer[String]()
+        val times = tdNodeList(week).text.replaceAll(" ", "").split("\n").filter(_ != "")
+        for (num <- 0 to times.size-1) {
+          timeList(week) += (rowNode \ "th").filter(_ \ "@class" contains Text("hour")).text + ":" +times(num)
+
         }
-        println()
-          /*
-          for ( currentMinute <- 0 to minuteList.size-1) {
-            timeList(currentWeek)(currentHour)(currentMinute) = hourNodeList(currentHour).text + ":" + minuteList(currentMinute)
-          }*/
-        //tdNodeList.foreach { tableData =>
-        
-        }
+      }
+
     }
-    /*
-    timeList.foreach { time =>
-    	time.foreach { smalltime =>
-    		smalltime.foreach { minutes =>
-    		  println(minutes.filter(_ != null))
-    		  
-    		}
-    	}
-    }*/
-    
-    
+    timeList.foreach { hour =>
+      hour.foreach { time =>
+        println(time)
+      }
+    }
+
     println("Hello Scala!")
   }
-  
+
   def toNode(str: String): Node = {
     val hp = new HtmlParser
     hp.setNamePolicy(XmlViolationPolicy.ALLOW)
